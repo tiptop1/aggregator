bad25445c89dc6f9bacb8529871c6710e7004236
tiptop <tomasz.gasior@gmail.com>
2025-11-05 23:01:06 +0100

    Implement main function.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use leprechaun::config::{read_configuration, ConfigError};
+use aggregator::config::{read_configuration, ConfigError};
+use aggregator::aggregator::{aggregate_fields, AggregatorError};
+use aggregator::printer::stdout_print;
 use std::path::PathBuf;
+use thiserror::Error;
+use tokio;
 
 
-fn main() -> Result<(), ConfigError> {
+#[derive(Error, Debug)]
+pub enum ApplicationError {
+    #[error("Failed to get configuration: {0}")]
+    Config(#[from] ConfigError),
+
+    #[error("Failed to aggregate fields: {0}")]
+    Aggregator(#[from] AggregatorError)
+
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), ApplicationError> {
     let mut path = PathBuf::new();
     path.push("config.toml");
     let config = read_configuration(&path)?;
-    print!("{:?}", config);
+    let aggregates = aggregate_fields(&config).await?;
+    stdout_print(&aggregates);
     Ok(())
 }
