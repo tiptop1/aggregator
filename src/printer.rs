70faeefec4aa04be7647fb8fb69d240c595b9427
tiptop <tomasz.gasior@gmail.com>
2025-11-05 23:00:43 +0100

    To suppress the warning, remove the brackets in the match statement.

--- src/printer.rs
+++ src/printer.rs
@@ -1,11 +1,11 @@
 use super::aggregator::Aggregates;
 
 
-fn stdout_print(aggregates: &Aggregates) {
+pub fn stdout_print(aggregates: &Aggregates) {
     for category in aggregates.categories() {
         print!("***** {} *****", category);
-        match(aggregates.fields(category)) {
-            Some(fields) => fields.iter().for_each(|k, v| print!("{}: {}", k, v)),
+        match aggregates.fields(category) {
+            Some(fields) => fields.iter().for_each(|(k, v)| print!("{}: {}", k, v)),
             _ => ()
         }
     }
