f319a2595770679e4bcec7b8c218d985ee35280a
tiptop <tomasz.gasior@gmail.com>
2025-12-11 22:07:21 +0100

    Improve print message.

--- src/printer.rs
+++ src/printer.rs
@@ -3,9 +3,9 @@ use super::aggregator::Aggregates;
 
 pub fn stdout_print(aggregates: &Aggregates) {
     for category in aggregates.categories() {
-        print!("***** {} *****", category);
+        print!("***** {} *****\n", category);
         match aggregates.fields(category) {
-            Some(fields) => fields.iter().for_each(|(k, v)| print!("{}: {}", k, v)),
+            Some(fields) => fields.iter().for_each(|(k, v)| print!("{}: {}\n", k, v)),
             _ => ()
         }
     }
